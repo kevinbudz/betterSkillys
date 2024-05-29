5376374e7b179ca6dbf89f17e95e80708244c4fe
runes02 <51036913+runes02@users.noreply.github.com>
2024-05-28 19:48:04 -0700

    Update Player.ItemEffects.cs

--- source/WorldServer/core/objects/player/Player.ItemEffects.cs
+++ source/WorldServer/core/objects/player/Player.ItemEffects.cs
@@ -35,6 +35,32 @@ namespace WorldServer.core.objects
                 //var item = Inventory[slot];
             }
         }
+
+        private void TryAddOnPlayerShootEffect()
+        {
+            for (var slot = 0; slot < 4; slot++)
+            {
+                //if (!CanApplySlotEffect(slot))
+                //    continue;
+
+                var item = Inventory[slot];
+                if (item == null)
+                    continue;
+
+                foreach (ActivateEffect eff in item.OnPlayerShootActivateEffects)
+                {
+                    if (eff.Proc != 0)
+                    {
+                        var rand = new Random();
+                        if (eff.Proc > rand.NextDouble())
+                            OnOtherActivate("shoot", item, Position);
+                    }
+                    else
+                        OnOtherActivate("shoot", item, Position);
+                }
+            }
+        }
+
         private void TryAddOnPlayerHitEffect()
         {
             for (var slot = 0; slot < 4; slot++)
