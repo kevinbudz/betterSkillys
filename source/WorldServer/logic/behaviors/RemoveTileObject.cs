a9c9ba81ad52c95133d5b3e9913a500ba9e00f8d
runes02 <51036913+runes02@users.noreply.github.com>
2024-06-03 16:12:27 -0700

    worlds for LoD, IceCave, IceTomb, Shaitan, and Ivory Wyvern

---
+++ source/WorldServer/logic/behaviors/RemoveTileObject.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Shared.resources;
+using WorldServer.core.net.datas;
+using WorldServer.core.objects;
+using WorldServer.core.structures;
+using WorldServer.core.worlds;
+using WorldServer.networking.packets.outgoing;
+using WorldServer.utils;
+
+namespace WorldServer.logic.behaviors
+{
+    internal class RemoveTileObject : Behavior
+    {
+        private readonly ushort _objType;
+        private readonly int _range;
+
+        public RemoveTileObject(ushort objType, int range)
+        {
+            _objType = objType;
+            _range = range;
+        }
+
+        protected override void OnStateEntry(Entity host, TickTime time, ref object state)
+        {
+            var objType = _objType;
+
+            var map = host.World.Map;
+
+            var w = map.Width;
+            var h = map.Height;
+
+            for (var y = 0; y < h; y++)
+                for (var x = 0; x < w; x++)
+                {
+                    var tile = map[x, y];
+
+                    if (tile.ObjType != objType)
+                        continue;
+
+                    var dx = Math.Abs(x - (int)host.X);
+                    var dy = Math.Abs(y - (int)host.Y);
+
+                    if (dx > _range || dy > _range)
+                        continue;
+
+                    tile.ObjType = 0;
+                    tile.UpdateCount++;
+                    map[x, y] = tile;
+                }
+        }
+
+        protected override void TickCore(Entity host, TickTime time, ref object state) { }
+    }
+}
---
+++ source/WorldServer/logic/behaviors/TeleportPlayer.cs
@@ -0,0 +1,33 @@
+﻿using WorldServer.core.objects;
+using WorldServer.core.worlds;
+using WorldServer.utils;
+
+namespace WorldServer.logic.behaviors
+{
+    class TeleportPlayer : CycleBehavior
+    {
+        private readonly float range;
+        private readonly float _X;
+        private readonly float _Y;
+        private readonly bool _isMapPosition;
+
+        public TeleportPlayer(double range, float X, float Y, bool isMapPosition = false)
+        {
+            this.range = (float)range;
+            _X = X;
+            _Y = Y;
+            _isMapPosition = isMapPosition;
+        }
+
+        protected override void TickCore(Entity host, TickTime time, ref object state)
+        {
+            Status = CycleStatus.NotStarted;
+            foreach (var i in host.GetNearestEntities(range, null, true))
+            {
+                var player = i as Player;
+                player?.TeleportPosition(time, _isMapPosition ? _X : host.X + _X, _isMapPosition ? _Y : host.Y + _Y, true);
+                state = CycleStatus.Completed;
+            }
+        }
+    }
+}
